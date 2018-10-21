1b363ad8f786ed674a2583365ed67fd8d732ff9f
Jodi Sloan <Jodi.Sloan@Shopify.com>
2018-10-21 15:45:43 -0400

    Add HR Analytics work

---
+++ HR-AnalyticsWorkshop/.ipynb_checkpoints/employee_churn-checkpoint.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 19 19:39:11 2018
+
+@author: jodisloan
+"""
+
+
+import pandas
+import matplotlib.pyplot as plot
+import seaborn as sns
+
+data = pandas.read_csv('HR_comma_sep.csv')
+data.head()
